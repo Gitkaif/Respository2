ac13ead92aced2c9dcd5d1820b28bc88d02f806e
sdac-9 <sdac-9@sdac-9-PC>
2024-09-10 19:14:17 +0530

    new file

--- Test1/src/com/pack1/Main2.java
+++ Test1/src/com/pack1/Main2.java
@@ -1,7 +1,7 @@
 package com.pack1;
 
 public class Main2 {
-	public static void main(String[] args) {
-		System.out.println("hello 2");
-	}
+public static void main(String[] args) {
+	System.out.println("2nd file");
+}
 }
